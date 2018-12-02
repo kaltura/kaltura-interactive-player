be8b2ebc54b541c56a5c92a44bda92998b521296
eitan <eitanavgil@kaltura.com>
2018-12-02 12:56:20 +0200

    CR notes - improve RaptConfig interface and shorter code of config

--- src/Kip.ts
+++ src/Kip.ts
@@ -4,13 +4,12 @@ import "script-loader!../libs/engineWithNodeId.min.js";
 import { VERSION } from "../version";
 
 declare var KalturaPlayer: any;
-// todo - consult Eran if it is worth to keep this interface or not.
 export interface RaptConfig {
   ui?: any;
   rapt: any;
   targetId: string;
   playback?: any;
-  sources?: any;
+  sources?: { poster?: any };
   provider?: any;
   plugins?: any;
 }
