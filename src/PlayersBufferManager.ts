0d810151cc1e0b299bd0c87a9cce58b5eb9c5f3d
lianbenjamin <79077248+lianbenjamin@users.noreply.github.com>
2021-05-13 10:14:56 +0300

    using typeof number instead of hasOwnProperty

--- src/PlayersBufferManager.ts
+++ src/PlayersBufferManager.ts
@@ -507,7 +507,7 @@ export class PlayersBufferManager extends Dispatcher {
 
         // find if there is any hotspot that has a startFrom attribute
         const hotspotsWithStartFrom = hotspots.filter((hs: any) => {
-            return hs.onClick && hs.onClick.find((itm: any) => itm.payload.hasOwnProperty("startFrom"));
+            return hs.onClick && hs.onClick.find((itm: any) => typeof itm.payload.startFrom === 'number');
         });
         const arrayToCache: RaptNode[] = [];
         for (const hotSpot of hotspots) {
@@ -541,7 +541,7 @@ export class PlayersBufferManager extends Dispatcher {
             // fill startFrom attributes if they match a hotspot 
             for (const hsWithStartTime of hotspotsWithStartFrom) {
                 // find if we have a relevant start hotspot 
-                const jumpingToData = hsWithStartTime.onClick.find((itm: any) => itm.payload.hasOwnProperty("startFrom")).payload;
+                const jumpingToData = hsWithStartTime.onClick.find((itm: any) => typeof itm.payload.startFrom === 'number').payload;
                 const matchingItem = arrayToCache.find((raptNode: RaptNode) => raptNode.id === jumpingToData.destination);
                 if (matchingItem) {
                     matchingItem.startFrom = jumpingToData.startFrom;
